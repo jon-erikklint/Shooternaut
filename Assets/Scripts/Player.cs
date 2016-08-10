c7cd70c783c6b1d91464552ab64d98e09c85c15f
unknown <jon-erik.klint@luukku.com>
2016-08-10 18:28:41 +0300

    minua pystyy kokeilemaan!!11!

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject projectile;
+    public float launchForce = 1f;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
+        transform.right = mousePosition - transform.position;
+        if (Input.GetMouseButtonDown(1))
+        {
+            Shoot(mousePosition);
+        }
+    }
+
+    void Shoot(Vector3 mousePos)
+    {
+        Vector3 dir = mousePos - transform.position;
+        dir /= dir.magnitude;
+        GameObject proj = Instantiate<GameObject>(projectile);
+        proj.transform.position = transform.position + dir*0.5f;
+        proj.GetComponent<Rigidbody2D>().velocity = rb.velocity;
+        proj.GetComponent<Rigidbody2D>().AddForce(launchForce * dir);
+        rb.AddForce(-launchForce * dir);
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
