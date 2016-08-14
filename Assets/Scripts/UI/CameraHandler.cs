cabd088d87bacfd849d98426bab895082122d89e
unknown <jon-erik.klint@luukku.com>
2016-08-14 19:03:38 +0300

    uudelleennimeämistä, järjestelyä ja kameran liikkuminen pelaajan mukana

---
+++ Assets/Scripts/UI/CameraHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraHandler : MonoBehaviour {
+
+    public float howFarAway;
+
+    private Transform playerTransform;
+
+    void Start()
+    {
+        playerTransform = FindObjectOfType<Player>().transform;
+    }
+    
+	void Update () {
+        Vector3 newPosition = playerTransform.position;
+        newPosition.z = howFarAway;
+
+        transform.position = newPosition;
+	}
+}
