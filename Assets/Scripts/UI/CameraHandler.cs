db6828f7b35827179f4d4abb26b5999661d71e90
Osama Abuzaid <usmiosama@hotmail.com>
2016-08-15 10:52:03 +0300

    Kameraa päivitetty

--- Assets/Scripts/UI/CameraHandler.cs
+++ Assets/Scripts/UI/CameraHandler.cs
@@ -3,19 +3,22 @@ using System.Collections;
 
 public class CameraHandler : MonoBehaviour {
 
-    public float howFarAway;
+    public float howFarAway = -10f;
+    public float smoothness = 7.0f;
 
     private Transform playerTransform;
 
     void Start()
     {
+        Debug.Log("Start!");
         playerTransform = FindObjectOfType<Player>().transform;
     }
     
-	void Update () {
-        Vector3 newPosition = playerTransform.position;
-        newPosition.z = howFarAway;
+	void FixedUpdate () {
+        Vector3 newPos = playerTransform.position;
+        newPos.z = howFarAway;
 
-        transform.position = newPosition;
-	}
+        transform.position += Vector3.Lerp(Vector3.zero, (newPos - transform.position), smoothness * Time.deltaTime);
+
+    }
 }
