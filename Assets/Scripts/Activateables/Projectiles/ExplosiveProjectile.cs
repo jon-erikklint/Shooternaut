18b9a3f598f9fe70a8146dc768e0eb9ff7d6e71e
Osama Abuzaid <usmiosama@hotmail.com>
2016-08-15 13:09:26 +0300

    kaBOOOOOM! kaBOOM BOOM BOOOOOM!

---
+++ Assets/Scripts/Activateables/Projectiles/ExplosiveProjectile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosiveProjectile : MonoBehaviour {
+
+    public GameObject explosion;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        GameObject boom = Instantiate(explosion);
+        boom.transform.position = transform.position;
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : Projectile {
+
+    public float radius = 3.0f;
+    public float energy = 50.0f;
+    public float duration = 0.5f;
+
+    private float birth;
+
+    private CircleCollider2D col;
+
+	// Use this for initialization
+	void Start () {
+        col = gameObject.GetComponent<CircleCollider2D>();
+        col.radius = 0;
+        col.isTrigger = true;
+        birth = Time.time;
+	}
+	
+    void Update()
+    {
+        float deltaTime = Time.time - birth;
+        col.radius = radius * deltaTime / duration;
+        if(deltaTime > duration)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Vector3 displacement = other.transform.position - transform.position;
+        other.GetComponent<Rigidbody2D>().AddForce(displacement/displacement.sqrMagnitude * energy/duration);
+    }
+
+}
