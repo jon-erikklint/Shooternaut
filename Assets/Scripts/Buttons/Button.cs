d619e9222676d95528bd8f123bb6ae693f952efb
Osama Abuzaid <usmiosama@hotmail.com>
2016-08-15 20:52:51 +0300

    push push push the button

---
+++ Assets/Scripts/Buttons/Button.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class Button : MonoBehaviour {
+
+    public UnityEvent onPress;
+    public List<string> tagList;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (tagList.Contains(col.gameObject.tag))
+            onPress.Invoke();
+    }
+}
---
+++ Assets/Scripts/Buttons/Destroyer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroyer : MonoBehaviour {
+    
+    public void Destroy()
+    {
+        Destroy(gameObject);
+    }
+
+}
