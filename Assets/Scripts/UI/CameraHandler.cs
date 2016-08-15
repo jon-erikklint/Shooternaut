30bea47818a7c5487916d2840aa549d97a78ba57
Osama Abuzaid <usmiosama@hotmail.com>
2016-08-15 11:34:00 +0300

    Kameraa päivitetty lisää

--- Assets/Scripts/UI/CameraHandler.cs
+++ Assets/Scripts/UI/CameraHandler.cs
@@ -5,20 +5,39 @@ public class CameraHandler : MonoBehaviour {
 
     public float howFarAway = -10f;
     public float smoothness = 7.0f;
+    public float mouseSensitivity = 0.2f;
+    public float maxLookDistance = 1.0f;
+    public bool inverse = false;
 
     private Transform playerTransform;
+    private Transform cameraTransform;
 
     void Start()
     {
         Debug.Log("Start!");
         playerTransform = FindObjectOfType<Player>().transform;
+        cameraTransform = transform.Find("Main Camera");
     }
     
 	void FixedUpdate () {
         Vector3 newPos = playerTransform.position;
         newPos.z = howFarAway;
+        
+        transform.position = Vector3.Lerp(transform.position, newPos, smoothness * Time.deltaTime);
+        if (inverse)
+        {
+            cameraTransform.localPosition = 2 * (newPos - transform.position);
+        }
+        else
+        {
+            cameraTransform.localPosition = Vector3.zero;
+        }
 
-        transform.position += Vector3.Lerp(Vector3.zero, (newPos - transform.position), smoothness * Time.deltaTime);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;
 
+        Vector3 lookingVector = mousePosition - playerTransform.position;
+
+        cameraTransform.localPosition += (lookingVector).normalized * Mathf.Min(lookingVector.magnitude*mouseSensitivity, maxLookDistance);
     }
 }
