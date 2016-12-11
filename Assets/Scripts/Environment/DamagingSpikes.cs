79946f5a6d976c53cd86048d23bd84efb28346d1
unknown <jon-erik.klint@luukku.com>
2016-12-11 16:01:14 +0200

    damagingSpikes

--- Assets/Scripts/Environment/DamagingSpikes.cs
+++ Assets/Scripts/Environment/DamagingSpikes.cs
@@ -1,13 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Spikes : MonoBehaviour {
+public class DamagingSpikes : MonoBehaviour {
+
+    public float damage;
 
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.collider.tag.Equals("Player"))
         {
-            col.gameObject.GetComponent<Player>().Destroy();
+            col.gameObject.GetComponent<Player>().LoseHealth(damage);
         }
     }
 }
---
+++ Assets/Scripts/Environment/DeathSpikes.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathSpikes : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.tag.Equals("Player"))
+        {
+            col.gameObject.GetComponent<Player>().Destroy();
+        }
+    }
+}
